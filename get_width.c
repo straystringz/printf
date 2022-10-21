c80e6caaa64294cbd511d756b72cacda246d6a43
Victor Emmanuel <62056934+straystringz@users.noreply.github.com>
2022-10-21 05:13:31 +0100

    Width

--- get_width.c
+++ get_width.c
@@ -5,10 +5,7 @@
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
- *
  * Return: width.
- *
  */
 int get_width(const char *format, int *i, va_list list)
 {
@@ -31,9 +28,6 @@ int get_width(const char *format, int *i, va_list list)
 		else
 			break;
 	}
-
 	*i = curr_i - 1;
-
 	return (width);
 }
-
